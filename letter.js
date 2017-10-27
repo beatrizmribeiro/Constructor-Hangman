5ba3537f326d7e8fe659a46b2d062bfb60e70e39
Beatriz Ribeiro <beatriz_mribeiro@hotmail.com>
2017-10-27 14:10:46 -0700

    updated letter.js

--- letter.js
+++ letter.js
@@ -1,5 +1,26 @@
-var Word = require("./word");
+var word = require("./word");
 
 var Letter = function (input) {
-	var wordUse = new
-}
+	var wordUse = new Word(input)
+	this.letters = wordUse.text.split("");
+	this.length = wordUse.length;
+	this.array = [];
+	this.blankSpace = function() {
+		for (var i=0; i < this.length; i++){
+			this.array.push("_");
+		}
+		console.log(this.array.join("  "));
+	}
+
+
+	this.blankReplace = function (answer){
+		for(var i = 0; i < this.length; i++){
+			if (this.letters[i] == answer){
+				this.array[i] = answer;
+			};
+		};
+	};
+};
+
+
+module.exports = letter;
